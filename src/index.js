49e420d637b3aee4354525ce68bdaa4641572edb
Anoop Varghese <anupvarghese@gmail.com>
2017-10-18 23:48:49 +1100

    add glamor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { css } from 'glamor';
+
+const wrapper = css({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gridGap: '10px',
+  fontSize: '40px',
+  gridAutoRows: '390px',
+  gridTemplateAreas: `". a a ."
+  ". a a ."`,
+});
+
+const item = css({
+  gridArea: 'a',
+  alignSelf: 'center',
+  justifySelf: 'center',
+});
 
 class Hello extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <div {...wrapper}>
+        <div {...item}>Hello {this.props.name}</div>
+      </div>
+    );
   }
 }
 
