29d2a5bbe25749b5e0606355f610464f1d2864d1
Anoop Varghese <anupvarghese@gmail.com>
2018-02-08 10:03:45 +1100

    add display component

---
+++ src/atoms/components/Display/index.js
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react';
+import { css } from 'glamor';
+import Input from '../../input';
+
+type displayT = {
+  value: number,
+};
+
+const Display = ({ value }: displayT) => {
+  return (
+    <div {...css({ display: 'flex', flexDirection: 'row' })}>
+      <Input
+        baseStyle={{
+          minHeight: '4rem',
+          flexBasis: '100%',
+          borderRadius: '4px',
+          border: '1px solid tomato',
+          fontSize: '1.5rem',
+          ':focus': {
+            outline: 'none !important',
+            border: '1px solid blue',
+            boxShadow: '0 0 10px #719ECE',
+            borderRadius: '4px',
+          },
+        }}
+        type="number"
+        disabled
+        value={value}
+      />
+    </div>
+  );
+};
+
+export default Display;
